7254198badeda52bc1d972acc67fff7a2b52b0fe
Divyang Modi <divyangmodi7@gmail.com>
2021-09-16 23:21:05 -0400

    Comments and minor changes

--- app/public/js/index.js
+++ app/public/js/index.js
@@ -3,26 +3,6 @@ const app=Vue.createApp({
         return{
             "person":{},
             "info":[
-                {
-                    "name": "Hope",
-                    "country":"USA",
-                    "birthday":"26 May",
-                    "age":"28",
-                    "email":"hope@example.com",
-                    "phone":"8976578779",
-                    "image": 'img/pic.jpeg'
-                    
-                },
-                {
-                    "name": "Ann",
-                    "country":"India",
-                    "birthday":"09 March",
-                    "age":"26",
-                    "email":"ann@example.com",
-                    "phone":"2342342343",
-                    "image": 'img/pic.jpeg'
-        
-                }
             ]
         }
     },
@@ -42,5 +22,3 @@ created(){
  
     }
 })
- 
-// Vue.creteApp(app).mount('#offerApp')
