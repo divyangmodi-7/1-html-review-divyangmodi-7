972511f5a8dbe60eedfdb5fe4e71376a686889fb
Divyang Modi <divyangmodi7@gmail.com>
2021-09-21 21:48:58 -0400

    Added refresh button which would call the API again

--- app/public/js/index.js
+++ app/public/js/index.js
@@ -11,6 +11,23 @@ const app=Vue.createApp({
             return dayjs(this.person.dob.date).format('D MMM YYYY')
         }  
     },
+    methods:{
+        fetchUserData(){
+
+            console.log("A")
+            fetch('https://randomuser.me/api/')
+            .then(response => response.json())
+            .then((responseJson) => {
+                console.log(responseJson);
+        
+                this.person =responseJson.results[0];
+            })
+        
+            .catch((err) => {
+                console.error(err);
+            })
+        }
+    },
 created(){
  
     fetch('https://randomuser.me/api/')
